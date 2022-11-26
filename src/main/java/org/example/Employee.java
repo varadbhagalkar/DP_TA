54484cf6b0523c6f7407b940fc14a9efe857fca9
viftergr <varadbhagalkar@gmail.com>
2022-11-26 15:27:10 +0530

    Second Commit

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,5 +1,32 @@
 package org.example;
 
-public class Employee {
 
+public abstract class Employee {
+    protected String name, eid, password;
+    protected int salary;
+    protected Employee supervisor;
+
+    Employee(String name, String eid, String password, int salary) {
+        this.name = name;
+        this.eid = eid;
+        this.password = password;
+        this.salary = salary;
+    }
+
+    public void setNextSupervisor(Employee supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public abstract void applyLeave(int numberOfDaysLeave);
+
+    public abstract void applyRaise(int amountOfRaise);
+
+    void ApproveLeave(String eid, int numberofDaysLeave) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void ApproveRaise(String eid, int amountOfRaise) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
+
--- src/main/java/org/example/EmployeeList.java
+++ src/main/java/org/example/EmployeeList.java
@@ -1,5 +1,55 @@
 package org.example;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class EmployeeList {
     //singleton for employee
+    ArrayList<Employee> empList=new ArrayList<>();
+    private EmployeeList(){}
+
+    private static EmployeeList singleton=new EmployeeList();
+
+    public void addEmp(Employee emp){
+        empList.add(emp);
+    }
+
+    public void removeEmp(String eid){
+        Iterator itr = empList.iterator();
+        while(itr.hasNext()){
+            String listEid=(String)itr.next();
+            if (eid==listEid){
+                itr.remove();
+            }
+        }
+    }
+
+    public void salEdit(String eid,int updatedSal){
+        Iterator itr = empList.iterator();
+        while(itr.hasNext()){
+            String listEid=(String)itr.next();
+            if (eid==listEid){
+                itr.salary=updatedSal;
+            }
+        }
+    }
+
+    public static EmployeeList returnList(){
+        return singleton;
+    }
+
+    public void edit(String name,String password,String eid){
+        Iterator itr = empList.iterator();
+        while(itr.hasNext()){
+            String listEid=(String)itr.next();
+            if (eid==listEid){
+                itr.name=name;
+                itr.password=password;
+            }
+        }
+    }
+
+
+
 }
